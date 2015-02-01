105f36523886c4d1af5618bb595fc0cb1e73357d
hoter <aytpaev@gmail.com>
2015-02-01 23:18:21 +0600

    last changes

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,15 @@
+/* Pseudo-class Player */
+function Player() {
+    
+}
+
+/* Pseudo-class Game */
+function Game(player1, player2, field) {
+    this.player1 = player1 || new Player;
+    this.player2 = player2 || new Player;
+    this.field = field || new Array(9);
+}
+
 onReady(function() {
    
 });
