9ee1b3ec421944d339ab7b2ae0adcf457941546f
hoter <aytpaev@gmail.com>
2015-02-01 16:43:35 +0600

    stylizing pages

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,24 @@
 onReady(function() {
-    var help = document.getElementById('help'),
-        helpPage = document.getElementById('help-page'),
+    var newGame = document.getElementById('new-game'),
+        game = document.getElementById('game'),
+        gamePage = document.getElementById('game-page');
+        menu = document.getElementById('menu'),
         menuPage = document.getElementById('menu-page');
+        help = document.getElementById('help'),
+        helpPage = document.getElementById('help-page'),
 
     help.addEventListener('click', function() {
         menuPage.style.display = 'none';
         helpPage.style.display = 'block';
+    });
+    
+    game.addEventListener('click', function() {
+        menuPage.style.display = 'none';
+        gamePage.style.display = 'table';
+    });
+    
+    newGame.addEventListener('click', function() {
+        menuPage.style.display = 'none';
+        gamePage.style.display = 'table';
     });
 });
