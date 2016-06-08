30fdbe53b34b59f7c21aa0c1a3db944a82727a79
hoter <aytpaev@gmail.com>
2016-06-08 15:47:01 +0600

    Game class: main entry point

---
+++ src/Game.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Game extends Component {
+  render() {
+    return (
+      <View>
+      	<Text>Tic tac toe</Text>
+      </View>
+    );
+  }
+}
+
+module.exports = Game;
